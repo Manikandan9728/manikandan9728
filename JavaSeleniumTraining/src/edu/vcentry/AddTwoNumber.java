3fb4a628e98b33798b3695171dd5b23fbe745e62
ELCOT <ELCOT@Lenovo>
2021-02-06 22:31:20 -0800

    Problem1

---
+++ JavaSeleniumTraining/src/edu/vcentry/AddTwoNumber.java
@@ -0,0 +1,34 @@
+package edu.vcentry;
+
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// step 1
+		double a= 20.0;
+		double b=100.0;
+		double c= a*b;
+		
+		// step 2
+		
+		double d= c % 20.0;
+		
+		// step 3
+
+		
+		double e=20.0+c;
+		
+		// step 4
+
+		
+		double f=e%40.0;
+		
+		// step 5
+		
+		double g=f*35.0 + (20);
+		
+		System.out.printf("Result %.2f \n", g);
+		
+	}
+
+}
