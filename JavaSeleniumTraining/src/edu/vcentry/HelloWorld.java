187547b5491ce279e1d378193e8e1b618e0e4fa8
ELCOT <ELCOT@Lenovo>
2021-02-08 02:57:14 -0800

    Feb-09-2021 Exercise2

--- JavaSeleniumTraining/src/edu/vcentry/HelloWorld.java
+++ JavaSeleniumTraining/src/edu/vcentry/HelloWorld.java
@@ -5,10 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double a=6d;
-		double b=2d;
-		
-		System.out.printf("The output is=%.2f", + (a / b));
+
+		System.out.print("Helloworld");
 
 	}
 
