fd279d3b2373c7b7727c75f8a994aa76e3a50149
ELCOT <ELCOT@Lenovo>
2021-02-09 05:05:38 -0800

    Feb-9-2021-Exercise_2

--- JavaSeleniumTraining/src/edu/vcentry/Exercise2.java
+++ JavaSeleniumTraining/src/edu/vcentry/Exercise2.java
@@ -4,15 +4,23 @@ public class Exercise2 {
 
 	public static void main(String[] args) {
 
-		int myNum1 = 50, myNum2 = 80, myNum3 = 95;
-
-		if (myNum1 > myNum2) {
+		int myNum1 = 160, myNum2 = 100, myNum3 = 95;
+		if ((myNum1 > myNum2) && (myNum1 > myNum3)) {
 			System.out.println(myNum1 + " is the greatest");
-		} else if (myNum2 > myNum3) {
+		} else if ((myNum2 > myNum3) && (myNum2 > myNum1)) {
 			System.out.println(myNum2 + " is the greatest");
-		} else if (myNum3 > myNum1) {
+		} else {
 			System.out.println(myNum3 + " is the greatest");
 		}
 
 	}
 }
+
+/*
+ * if (myNum1 > myNum2) { System.out.println(myNum1 + " is the greatest"); }
+ * else if (myNum2 > myNum3) { System.out.println(myNum2 + " is the greatest");
+ * }
+ * 
+ * else if (myNum3 > myNum1) { System.out.println(myNum3 + " is the greatest");
+ * }
+ */
